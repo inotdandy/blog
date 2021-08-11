d41ddcfaca4694b05d37b0dacb89155f4b8797ad
Dandy Inot <dandy.inot1990@gmail.com>
2021-08-12 00:00:42 +0800

    ongoing

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
@@ -13,7 +14,9 @@ class PostController extends Controller
      */
     public function index()
     {
-        return view('blog.index');
+        return view('blog.index', [
+            'posts' => Post::orderBy('created_at', 'desc')->with('user')->get()
+        ]);
     }
 
     /**
@@ -23,7 +26,7 @@ class PostController extends Controller
      */
     public function create()
     {
-        //
+        return view('blog.create');
     }
 
     /**
